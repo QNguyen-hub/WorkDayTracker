d4baa096ef1b2ee10710b14c2e77fbe54d0044a4
Quy Nguyen <qnguyen303@gmail.com>
2022-05-21 22:13:53 -0600

    was able to get the textarea block to change color base on time

--- script.js
+++ script.js
@@ -1,7 +1,94 @@
 var currentDate = document.querySelector("#currentDay");
-
+var time = document.querySelector(".hour");
+var currentTime = moment().format("HH");
+var eight = document.querySelector("#eightam");
+var nine = document.querySelector("#nineam");
+var ten = document.querySelector("#tenam");
+var eleven = document.querySelector("#elevenam");
+var twelve = document.querySelector("#twelvepm");
+var one = document.querySelector("#onepm");
+var two = document.querySelector("#twopm");
+var three = document.querySelector("#threepm");
+var four = document.querySelector("#fourpm");
+var five = document.querySelector("#fivepm");
 var timer = function () {
   var date = moment().format("MMMM Do YYYY, h:mm:ss a");
   currentDate.textContent = date;
 };
 setInterval(timer, 1000);
+
+function colorBlock() {
+  if (currentTime == 8) {
+    eight.setAttribute("class", "present");
+  } else if (currentTime > 8) {
+    eight.setAttribute("class", "past");
+  } else if (currentTime < 8) {
+    eight.setAttribute("class", "future");
+  }
+  if (currentTime == 9) {
+    nine.setAttribute("class", "present");
+  } else if (currentTime > 9) {
+    nine.setAttribute("class", "past");
+  } else if (currentTime < 9) {
+    nine.setAttribute("class", "future");
+  }
+  if (currentTime == 10) {
+    ten.setAttribute("class", "present");
+  } else if (currentTime > 10) {
+    ten.setAttribute("class", "past");
+  } else if (currentTime < 10) {
+    ten.setAttribute("class", "future");
+  }
+  if (currentTime == 11) {
+    eleven.setAttribute("class", "present");
+  } else if (currentTime > 11) {
+    eleven.setAttribute("class", "past");
+  } else if (currentTime < 11) {
+    eleven.setAttribute("class", "future");
+  }
+  if (currentTime == 12) {
+    twelve.setAttribute("class", "present");
+  } else if (currentTime > 12) {
+    twelve.setAttribute("class", "past");
+  } else if (currentTime < 12) {
+    twelve.setAttribute("class", "future");
+  }
+  if (currentTime == 1) {
+    one.setAttribute("class", "present");
+  } else if (currentTime > 1) {
+    one.setAttribute("class", "past");
+  } else if (currentTime < 1) {
+    one.setAttribute("class", "future");
+  }
+  if (currentTime == 2) {
+    two.setAttribute("class", "present");
+  } else if (currentTime > 2) {
+    two.setAttribute("class", "past");
+  } else if (currentTime < 2) {
+    two.setAttribute("class", "future");
+  }
+  if (currentTime == 3) {
+    three.setAttribute("class", "present");
+  } else if (currentTime > 3) {
+    three.setAttribute("class", "past");
+  } else if (currentTime < 3) {
+    three.setAttribute("class", "future");
+  }
+  if (currentTime == 4) {
+    four.setAttribute("class", "present");
+  } else if (currentTime > 4) {
+    four.setAttribute("class", "past");
+  } else if (currentTime < 4) {
+    four.setAttribute("class", "future");
+  }
+  if (currentTime == 5) {
+    five.setAttribute("class", "present");
+  } else if (currentTime > 5) {
+    five.setAttribute("class", "past");
+  } else if (currentTime < 5) {
+    five.setAttribute("class", "future");
+  }
+}
+
+console.log(currentTime);
+colorBlock();
